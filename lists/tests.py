70713c54f9ffc45c692887690f64e29df31ee119
immortals <550413470@qq.com>
2016-08-08 23:41:30 +0800

    在重构functional_test之前的提交

--- lists/tests.py
+++ lists/tests.py
@@ -13,5 +13,19 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expect_html = render_to_string('home.html')
+        expect_html = render_to_string('home.html', request=request)
+        self.assertEqual(response.content.decode(), expect_html)
+
+    def test_home_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        expect_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'},
+            request=request
+        )
+
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
         self.assertEqual(response.content.decode(), expect_html)
--- lists/views.py
+++ lists/views.py
@@ -4,5 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-    # return HttpResponse('<html><title>To-Do lists</title></html>')
+    if request.method == 'POST':
+        return render(
+            request,
+            'home.html',
+            {
+                'new_item_text': request.POST.get('item_text', ''),
+            }
+        )
     return render(request, 'home.html')
