658eef7bae91f308f8e457840f9c7c4cf28acdf0
immortals <550413470@qq.com>
2016-08-08 18:48:20 +0800

    编写了首个unittest 并且使用注释编写了首个功能测试

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(2)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 依迪丝听说有个很酷的在线代办事项应用
+        # 她去看了这个应用的首页
+        self.browser.get('http://localhost:8000')
+
+        # 她注意到网页的标题和头部都包含“To-Do”这个词
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
