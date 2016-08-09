e807c1dc572c8cdc699ac04afa2cd04bd745412e
immortals <550413470@qq.com>
2016-08-09 12:42:29 +0800

    在POST后重定向，并且在模板中显示全部的项目

--- functional_tests.py
+++ functional_tests.py
@@ -36,9 +36,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
-        self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
-        
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
 
         self.fail('Finish the test')
 
