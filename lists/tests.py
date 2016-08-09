6cbe9a459e5a41411641b5db264869162cb6ca87
immortals <550413470@qq.com>
2016-08-09 10:49:45 +0800

    为List创建了Item模型并关联了migration

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,11 @@ from django.test import TestCase, Client
 from django.core.urlresolvers import reverse, resolve
 from django.template.loader import render_to_string
 from django.http import HttpRequest
+
 from lists.views import home_page
+from lists.models import Item
+
+
 
 class HomePageTest(TestCase):
 
@@ -29,3 +33,19 @@ class HomePageTest(TestCase):
         response = home_page(request)
         self.assertIn('A new list item', response.content.decode())
         self.assertEqual(response.content.decode(), expect_html)
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_item(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        self.assertEqual(saved_items[0].text, 'The first (ever) list item')
